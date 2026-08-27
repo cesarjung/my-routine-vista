d7d35bc86eedbd0efbfea0f6fd182b934954b6ea
Cesar Jung <cesar.jung@sirtec.com.br>
2026-08-27 15:30:22 -0300

    fix: adicionar projetos ao ComputedMapData para nao quebrar popup do Leaflet

--- src/components/views/PlanejamentoEquipesMap.tsx
+++ src/components/views/PlanejamentoEquipesMap.tsx
@@ -55,7 +55,7 @@ export interface PlanejamentoEquipesMapProps {
 export interface ComputedMapData {
   equipe: string;
   color: string;
-  points: Array<{ lat: number; lng: number; num: number; municipio: string; count: number; date: Date }>;
+  points: Array<{ lat: number; lng: number; num: number; municipio: string; count: number; date: Date; projetos: string[] }>;
 }
 
 /**
@@ -91,7 +91,7 @@ export function computeMapData(
         finalLng += visits * 0.015;
       }
       visitedCoords.set(group.latLngKey, visits + 1);
-      points.push({ lat: finalLat, lng: finalLng, num: seqNum++, municipio: group.municipio, count: group.count, date: group.date });
+      points.push({ lat: finalLat, lng: finalLng, num: seqNum++, municipio: group.municipio, count: group.count, date: group.date, projetos: group.projetos });
     };
 
     (row.atividadesDiarias || []).forEach(ativ => {
